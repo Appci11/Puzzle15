746795452c558c225c6c1dd64ca52d55d402eab1
Appci1 <Rolkaappci@gmail.com>
2021-10-03 14:54:58 +0300

    Added main menu

--- app/src/main/java/com/puzzle15/MainActivity.java
+++ app/src/main/java/com/puzzle15/MainActivity.java
@@ -2,13 +2,59 @@ package com.puzzle15;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnRandomGame, btnCustomGame, btnSettings, btnAbout;
+    private ImageView imgLogo;  //Jei sumastytume pakeist keiciant "Theme"
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+
+        btnRandomGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RandomGameActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnCustomGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CustomGameActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initViews() {
+        btnRandomGame = findViewById(R.id.btnRandomGame);
+        btnCustomGame = findViewById(R.id.btnCustomGame);
+        btnSettings = findViewById(R.id.btnSettings);
+        btnAbout = findViewById(R.id.btnAbout);
+        imgLogo = findViewById(R.id.imgLogo);
     }
 }
