4108555299fd75f5bb0738f4fb13257c8b635c44
Appci1 <Rolkaappci@gmail.com>
2021-10-03 22:05:14 +0300

    Worked on Custom Game UI

--- app/src/main/java/com/puzzle15/CustomGameActivity.java
+++ app/src/main/java/com/puzzle15/CustomGameActivity.java
@@ -2,13 +2,40 @@ package com.puzzle15;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CustomGameActivity extends AppCompatActivity {
+
+    private Button btnStartCustomGame, StartAIPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_game);
+
+        initViews();
+
+        btnStartCustomGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CustomGameActivity.this, RandomGameActivity.class);
+                startActivity(intent);
+            }
+        });
+        StartAIPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CustomGameActivity.this, RandomGameActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initViews() {
+        btnStartCustomGame = findViewById(R.id.btnStartCustomGame);
+        StartAIPlay = findViewById(R.id.StartAIPlay);
     }
 }
